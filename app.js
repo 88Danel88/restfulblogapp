317b0c4a82e2c10f075851b097d774c4464c057a
daneltabada <daneltabada@gmail.com>
2018-09-15 15:47:17 -0400

    added environment variable

--- app.js
+++ app.js
@@ -6,10 +6,8 @@ var express = require ('express'),
     app = express ();
 
 // APP CONFIG
-//local
-//mongoose.connect("mongodb://localhost:27017/restful_blog_app", { useNewUrlParser: true });
-//mLab
-mongoose.connect("mongodb://danel:1babylove@ds243212.mlab.com:43212/restful_blog_app",{ useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL,{ useNewUrlParser: true });
+console.log(process.env.DATABASEURL);
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
